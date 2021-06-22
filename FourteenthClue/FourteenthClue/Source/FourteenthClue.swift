7ffc974f61ab75f69ec86f7332bfef4eb62f5b84
Joseph Roque <github@josephroque.ca>
2021-06-21 22:35:46 -0700

    Initial project commit

---
+++ FourteenthClue/FourteenthClue/Source/FourteenthClue.swift
@@ -0,0 +1,19 @@
+//
+//  FourteenthClue.swift
+//  Fourteenth Clue
+//
+//  Created by Joseph Roque on 2021-06-21.
+//
+
+import SwiftUI
+
+@main
+struct FourteenthClue: App {
+
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+
+}
